c8d9fbfab59db9965cc1432050c08c88bf7af129
clivon254 <clivon254@icloud.com>
2026-05-26 10:04:04 +0300

    implementation and the documentation of authcontext

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './redux'
+import { AuthProvider } from './contexts/AuthContext'
 import './index.css'
 import App from './App.tsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
+
     <Provider store={store}>
+
       <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
+
+        <BrowserRouter>
+
+          <AuthProvider>
+
+            <QueryClientProvider client={queryClient}>
+
+              <App />
+              
+            </QueryClientProvider>
+
+          </AuthProvider>
+
+        </BrowserRouter>
+
       </PersistGate>
+
     </Provider>
+
   </StrictMode>,
 )
